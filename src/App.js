c15f69f149986f20a5b2b9e5283f5f4a2b51c1b1
Cody Brunner <rockchalkwushock@icloud.com>
2018-05-03 05:41:21 -0500

    chore(root): initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <View>
+        <Text>App.js</Text>
+      </View>
+    )
+  }
+}
+
+export default App
