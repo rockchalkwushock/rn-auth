7fb4e730e8063cd4f7411d88c1fcd52af058e4f4
Cody Brunner <rockchalkwushock@icloud.com>
2018-05-03 05:51:36 -0500

    feat(root): adds common components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import { Header } from './components'
+
 class App extends Component {
   state = {}
   render() {
     return (
       <View>
+        <Header text="Auth" />
         <Text>App.js</Text>
       </View>
     )
---
+++ src/components/commons/index.js
@@ -0,0 +1,6 @@
+import Button from './Button'
+import Card from './Card'
+import Header from './Header'
+import Section from './Section'
+
+export { Button, Card, Header, Section }
